c1b61cfeb3726c7ed7b6f0f096df99b020ce31f1
pgenfer <pgenfer@gmail.com>
2016-12-07 00:44:04 +0100

    [fix] lib folder was ignored

---
+++ umlsketch.lib/DomainObject/DiagramColorPalette.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UmlSketch.DomainObject
+{
+    /// <summary>
+    /// returns all colors that are currently
+    /// available in the diagram
+    /// </summary>
+    public class DiagramColorPalette
+    {
+        private readonly Diagram _diagram;
+
+        public DiagramColorPalette(Diagram diagram)
+        {
+            _diagram = diagram;
+        }
+
+        public IEnumerable<string> CollectColorsUsedInDiagram()
+        {
+            var colors = _diagram.Classifiers
+                .Select(x => x.Color)
+                .Concat(_diagram.Classifiers.Select(x => x.Note.Color))
+                .Concat(new[] {_diagram.Note.Color})
+                .Distinct()
+                .Where(x => !string.IsNullOrEmpty(x)); // skip transparent colors
+            return colors;
+        }
+    }
+}
